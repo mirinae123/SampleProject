6a78f4827d33e4a196b1ef5c2746b4df6fa64625
SushiJoa <127503756+SushiJoa@users.noreply.github.com>
2023-07-12 10:24:50 +0900

    플레이어이동액션보물과충돌구현

--- SampleProject/Assets/Scripts/PlayerDig.cs
+++ SampleProject/Assets/Scripts/PlayerDig.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class githubTest : MonoBehaviour
+public class PlayerDig : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,6 @@ public class githubTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
+
 }
