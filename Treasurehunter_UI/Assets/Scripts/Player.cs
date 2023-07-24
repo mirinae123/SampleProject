531d9288ecfbe4b4aeb2dde401f6664ff55b1378
mirinae123 <137975323+mirinae123@users.noreply.github.com>
2023-07-24 13:37:29 +0900

    전체 업데이트

---
+++ Treasurehunter_UI/Assets/Scripts/Player.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+
+    Rigidbody2D rigid;
+    public float speed;
+
+    public static float maxHealth = 100f; // 최대 체력
+    public static float currentHealth = 100f; // 현재 체력
+
+    public bool isMovable;
+    public bool isAction;
+
+    private GameObject treasure;
+
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+
+        isMovable = true;
+        isAction = false;
+
+        treasure = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(KeyCode.Z) && !isAction)
+        {
+            isMovable = false;
+            isAction = true;
+            AudioManager.instance.PlaySfx(AudioManager.SFX.Dig);
+            Invoke("Dig", 1);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+
+        if(isMovable)
+            rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    void Dig()
+    {
+        if (treasure)
+        {
+            Debug.Log("땅파기 성공");
+            treasure.GetComponent<Treasure>().Find();
+            AudioManager.instance.PlaySfx(AudioManager.SFX.Success);
+            currentHealth += 10; // 성공시 체력 회복
+            Debug.Log(currentHealth);
+        }
+        else
+        {
+            Debug.Log("땅파기 실패");
+            AudioManager.instance.PlaySfx(AudioManager.SFX.Fail);
+            currentHealth -= 10; // 실패시 체력 감소
+            Debug.Log(currentHealth);
+        }
+        isAction = false;
+        isMovable = true;
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Treasure"))
+        {
+            Debug.Log("충돌");
+            treasure = col.gameObject;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Treasure"))
+        {
+            Debug.Log("충돌 나감");
+            treasure = null;
+        }
+    }
+
+}
